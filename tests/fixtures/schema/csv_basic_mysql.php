f9d25c675502a6c1df3cacc0e029c08853579629
Zach Watkins <3458875+ZachWatkins@users.noreply.github.com>
2026-08-29 19:33:01 -0500

    Add columns to fixtures

--- tests/fixtures/schema/csv_basic_mysql.php
+++ tests/fixtures/schema/csv_basic_mysql.php
@@ -16,4 +16,10 @@ return new SqlColumnCollection([
     new SqlColumn('name', MySqlColumnType::Varchar->value, [
         ColumnModifier::Unique,
     ]),
+    new SqlColumn('birthday', MySqlColumnType::Date->value, []),
+    new SqlColumn('created_at', MySqlColumnType::DateTime->value, []),
+    new SqlColumn('accept_terms', MySqlColumnType::Boolean->value, []),
+    new SqlColumn('deleted_at', MySqlColumnType::DateTime->value, [
+        ColumnModifier::Nullable,
+    ]),
 ]);

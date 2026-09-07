d8f10a2bcd79d7aaeaef8f419705b3a7597efe17
Zach Watkins <3458875+ZachWatkins@users.noreply.github.com>
2026-09-07 11:14:57 -0500

    Update MySqlColumnType documentation to reference MySQL data type links

--- src/Enums/MySqlColumnType.php
+++ src/Enums/MySqlColumnType.php
@@ -14,25 +14,31 @@ enum MySqlColumnType: string
     /**
      * Unsigned range: 0 to 255
      * Signed range: -128 to 127
-     * @see https://learn.microsoft.com/en-us/sql/t-sql/data-types/int-bigint-smallint-and-tinyint-transact-sql
+     * @see https://dev.mysql.com/doc/refman/8.0/en/integer-types.html
      */
     case TinyInt = 'TINYINT';
     /**
      * Unsigned range: 0 to 65,535
      * Signed range: -32,768 to 32,767
-     * @see https://learn.microsoft.com/en-us/sql/t-sql/data-types/int-bigint-smallint-and-tinyint-transact-sql
+     * @see https://dev.mysql.com/doc/refman/8.0/en/integer-types.html
      */
     case SmallInt = 'SMALLINT';
     /**
      * Unsigned range: 0 to 4,294,967,295
      * Signed range: -2,147,483,648 to 2,147,483,647
-     * @see https://learn.microsoft.com/en-us/sql/t-sql/data-types/int-bigint-smallint-and-tinyint-transact-sql
+     * @see https://dev.mysql.com/doc/refman/8.0/en/integer-types.html
      */
     case Int = 'INT';
     /**
+     * Unsigned range: 0 to 16,777,215
+     * Signed range: -8,388,608 to 8,388,607
+     * @see https://dev.mysql.com/doc/refman/8.0/en/integer-types.html
+     */
+    case MediumInt = 'MEDIUMINT';
+    /**
      * Unsigned range: 0 to 18,446,744,073,709,551,615
      * Signed range: -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
-     * @see https://learn.microsoft.com/en-us/sql/t-sql/data-types/int-bigint-smallint-and-tinyint-transact-sql
+     * @see https://dev.mysql.com/doc/refman/8.0/en/integer-types.html
      */
     case BigInt = 'BIGINT';
     /**

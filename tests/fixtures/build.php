3050045c855458e884a5a047dada0c47dfe5ddda
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2026-09-07 18:29:21 -0500

    Add test data build file

---
+++ tests/fixtures/build.php
@@ -0,0 +1,28 @@
+<?php
+
+use Tests\Fixtures\Build\TestDataGenerator;
+use Tests\Fixtures\Build\File\TestCsvFileGenerator;
+use Tests\Fixtures\Build\File\TestHttpFileGenerator;
+use Tests\Fixtures\Build\File\TestJsonFileGenerator;
+use Tests\Fixtures\Build\File\TestXlsxFileGenerator;
+use Tests\Fixtures\Build\File\TestXmlFileGenerator;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+$generator = new TestDataGenerator();
+$data = $generator->generate(10);
+
+$csvGenerator = new TestCsvFileGenerator();
+$csvGenerator->generate('test.csv', $data);
+
+$httpGenerator = new TestHttpFileGenerator();
+$httpGenerator->generate('test.http', $data);
+
+$jsonGenerator = new TestJsonFileGenerator();
+$jsonGenerator->generate('test.json', $data);
+
+$xlsxGenerator = new TestXlsxFileGenerator();
+$xlsxGenerator->generate('test.xlsx', $data);
+
+$xmlGenerator = new TestXmlFileGenerator();
+$xmlGenerator->generate('test.xml', $data);

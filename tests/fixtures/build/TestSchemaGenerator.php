653618c40380d11d9e4687b18e29e4ab453b68de
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2026-09-12 12:33:41 -0500

    Add SQLite schema generation method to TestSchemaGenerator

--- tests/fixtures/build/TestSchemaGenerator.php
+++ tests/fixtures/build/TestSchemaGenerator.php
@@ -9,6 +9,7 @@ namespace Tests\Fixtures\Build;
 use ZachWatkins\InferDataSchema\Parsers\JsonParser;
 use ZachWatkins\InferDataSchema\Enums\ColumnModifier;
 use ZachWatkins\InferDataSchema\Enums\MySqlColumnType;
+use ZachWatkins\InferDataSchema\Enums\SqliteColumnType;
 
 class TestSchemaGenerator
 {
@@ -78,6 +79,84 @@ class TestSchemaGenerator
                 default => throw new \InvalidArgumentException('Unknown column type: ' . $column->getType()),
             };
             $output[] = '    new SqlColumn(\'' . $column->getName() . '\', MySqlColumnType::' . $columnType . '->value';
+            $modifiers = $column->getModifiers();
+            if (!empty($modifiers)) {
+                $output[array_key_last($output)] .= ', [';
+                foreach ($column->getModifiers() as $modifier) {
+                    $mod = match ($modifier) {
+                        ColumnModifier::Unique => 'Unique',
+                        ColumnModifier::Unsigned => 'Unsigned',
+                        ColumnModifier::AutoIncrement => 'AutoIncrement',
+                        ColumnModifier::Nullable => 'Nullable',
+                        default => throw new \InvalidArgumentException('Unknown modifier: ' . $modifier->value),
+                    };
+                    $output[] = '        ColumnModifier::' . $mod . ',';
+                }
+                $output[array_key_last($output)] .= "\n    ]";
+            }
+            $output[array_key_last($output)] .= '),';
+        }
+        $output[] = ']);';
+        $output[] = '';
+
+        file_put_contents(__DIR__ . '/../schema/' . $schemaFileName, implode("\n", $output));
+    }
+
+    public function generateSqlite(string $dataFileName, string $schemaFileName): void
+    {
+        $parser = new JsonParser();
+        $directory = __DIR__ . '/../data/';
+        $filePath = $directory . $dataFileName;
+        $schema = $parser->parse($filePath, 'sqlite');
+        $columns = $schema->getColumns();
+
+        // Example output:
+        // declare(strict_types=1);
+
+        // use ZachWatkins\InferDataSchema\Enums\ColumnModifier;
+        // use ZachWatkins\InferDataSchema\Enums\MySqlColumnType;
+        // use ZachWatkins\InferDataSchema\Models\SqlColumn;
+        // use ZachWatkins\InferDataSchema\Models\SqlColumnCollection;
+
+        // return new SqlColumnCollection([
+        //     new SqlColumn('id', SqliteColumnType::TinyInt->value, [
+        //         ColumnModifier::Unique,
+        //         ColumnModifier::Unsigned,
+        //         ColumnModifier::AutoIncrement,
+        //     ]),
+        //     new SqlColumn('name', SqliteColumnType::Varchar->value, [
+        //         ColumnModifier::Unique,
+        //     ]),
+        //     new SqlColumn('birthday', SqliteColumnType::Date->value, []),
+        //     new SqlColumn('created_at', SqliteColumnType::DateTime->value, []),
+        //     new SqlColumn('accept_terms', SqliteColumnType::Boolean->value, []),
+        //     new SqlColumn('deleted_at', SqliteColumnType::DateTime->value, [
+        //         ColumnModifier::Nullable,
+        //     ]),
+        // ]);
+
+        $output = [
+            '<?php',
+            '',
+            'declare(strict_types=1);',
+            '',
+            'use ZachWatkins\InferDataSchema\Enums\ColumnModifier;',
+            'use ZachWatkins\InferDataSchema\Enums\SqliteColumnType;',
+            'use ZachWatkins\InferDataSchema\Models\SqlColumn;',
+            'use ZachWatkins\InferDataSchema\Models\SqlColumnCollection;',
+            '',
+            'return new SqlColumnCollection([',
+        ];
+
+        foreach ($columns as $column) {
+            $columnType = match ($column->getType()) {
+                SqliteColumnType::Integer->value => 'Integer',
+                SqliteColumnType::Numeric->value => 'Numeric',
+                SqliteColumnType::Real->value => 'Real',
+                SqliteColumnType::Text->value => 'Text',
+                default => throw new \InvalidArgumentException('Unknown column type: ' . $column->getType()),
+            };
+            $output[] = '    new SqlColumn(\'' . $column->getName() . '\', SqliteColumnType::' . $columnType . '->value';
             $modifiers = $column->getModifiers();
             if (!empty($modifiers)) {
                 $output[array_key_last($output)] .= ', [';

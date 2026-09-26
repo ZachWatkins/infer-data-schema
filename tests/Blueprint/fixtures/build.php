0b28587204aa7a58edf46d565616ae25990e811b
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2026-09-25 21:23:58 -0500

    Update the source directory for data files and rebuild the files without the bit column

--- tests/Blueprint/fixtures/build.php
+++ tests/Blueprint/fixtures/build.php
@@ -15,19 +15,19 @@ foreach ($existingFiles as $file) {
     unlink($file);
 }
 $blueprintGenerator = new TestBlueprintGenerator();
-$blueprintGenerator->generate(__DIR__ . '/../../fixtures/data/test_mysql.json', $outDir . 'test_mysql_basic.yaml');
-$blueprintGenerator->generate(__DIR__ . '/../../fixtures/data/test_mysql.json', $outDir . 'test_mysql_inertia_basic.yaml', new BlueprintConfig(
+$blueprintGenerator->generate(__DIR__ . '/../../SQL/fixtures/data/test_mysql.json', $outDir . 'test_mysql_basic.yaml');
+$blueprintGenerator->generate(__DIR__ . '/../../SQL/fixtures/data/test_mysql.json', $outDir . 'test_mysql_inertia_basic.yaml', new BlueprintConfig(
     view: BlueprintConfigView::Inertia,
     resources: [BlueprintConfigResource::Web],
     seeders: true
 ));
-$blueprintGenerator->generate(__DIR__ . '/../../fixtures/data/test_mysql.json', $outDir . 'test_mysql_inertia_view_only.yaml', new BlueprintConfig(
+$blueprintGenerator->generate(__DIR__ . '/../../SQL/fixtures/data/test_mysql.json', $outDir . 'test_mysql_inertia_view_only.yaml', new BlueprintConfig(
     view: BlueprintConfigView::Inertia,
     resources: [BlueprintConfigResource::Index, BlueprintConfigResource::Show]
 ));
-$blueprintGenerator->generate(__DIR__ . '/../../fixtures/data/test_mysql.json', $outDir . 'test_mysql_crud_with_custom_methods.yaml', new BlueprintConfig(
+$blueprintGenerator->generate(__DIR__ . '/../../SQL/fixtures/data/test_mysql.json', $outDir . 'test_mysql_crud_with_custom_methods.yaml', new BlueprintConfig(
     methods: array_merge(BlueprintConfigResource::webMethods(), ['customMethod'])
 ));
-$blueprintGenerator->generate(__DIR__ . '/../../fixtures/data/test_mysql.json', $outDir . 'test_mysql_api_methods.yaml', new BlueprintConfig(
+$blueprintGenerator->generate(__DIR__ . '/../../SQL/fixtures/data/test_mysql.json', $outDir . 'test_mysql_api_methods.yaml', new BlueprintConfig(
     methods: BlueprintConfigResource::apiMethods()
 ));
